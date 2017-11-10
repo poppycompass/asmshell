184c61b169a0e288a802f89984bd7a28ef3f1806
poppycompass <t0g0v31dk@gmail.com>
2017-11-11 02:28:36 +0900

    add sparc64 support

--- go/main.go
+++ go/main.go
@@ -80,7 +80,7 @@ func setArch(strArch string) arch.Machine {
         case "mips64eb"    : mc = arch.SetMips64(true)  // fixme: something wrong?
         case "sparc"       : mc = arch.SetSparc(true)   // sparc standard is big-endian
         case "sparcel"     : mc = arch.SetSparc(false)  // assemble only, unicorn: UNSUPPORTED, keystone: supported
-//        case "sparc64"     : mc = arch.SetSparc64(asmsh, true) // fixme: something wrong?, sparc standard is big-endian
+        case "sparc64"     : mc = arch.SetSparc64(true) // fixme: something wrong?, sparc standard is big-endian
 //        case "ppc",
 //             "powerpc"     : mc = arch.SetPowerPC(asmsh, true)
 //        case "ppc64",
