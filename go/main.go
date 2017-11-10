0e75569b4008105413d926e102def30e3dd9faf6
poppycompass <t0g0v31dk@gmail.com>
2017-11-11 01:59:58 +0900

    add mips64(eb) support

--- go/main.go
+++ go/main.go
@@ -76,8 +76,8 @@ func setArch(strArch string) arch.Machine {
         case "arm64eb"     : mc = arch.SetArm64(true)   // TODO: test, fixme: something wrong?
         case "mips"        : mc = arch.SetMips(false)
         case "mipseb"      : mc = arch.SetMips(true)
-//        case "mips64"      : mc = arch.SetMips64(asmsh, false) // fixme: something wrong?
-//        case "mips64eb"    : mc = arch.SetMips64(asmsh, true)  // fixme: something wrong?
+        case "mips64"      : mc = arch.SetMips64(false) // fixme: something wrong?
+        case "mips64eb"    : mc = arch.SetMips64(true)  // fixme: something wrong?
 //        case "sparc"       : mc = arch.SetSparc(asmsh, true)   // sparc standard is big-endian
 //        case "sparcel"     : mc = arch.SetSparc(asmsh, false)  // assemble only, unicorn: UNSUPPORTED, keystone: supported
 //        case "sparc64"     : mc = arch.SetSparc64(asmsh, true) // fixme: something wrong?, sparc standard is big-endian
