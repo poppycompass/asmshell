8d82adac835385ff9ea5d209a29b0e55d420bbf0
poppycompass <t0g0v31dk@gmail.com>
2017-11-11 03:12:16 +0900

    add arm64 support(little endian only)

--- go/arch/arm64.go
+++ go/arch/arm64.go
@@ -3,39 +3,44 @@ package arch
 import (
     "github.com/keystone-engine/keystone/bindings/go/keystone"
     uc "github.com/unicorn-engine/unicorn/bindings/go/unicorn"
-    as "github.com/poppycompass/asmshell/go"
 )
 
 // 'AArch64'? ref: bindings/vb6/mKeystone.bas:ARM-64, also called AArch64
-func SetArm64(asmsh *as.AsmShell, bigEndian bool) {
-    asmsh.CodeAddr  = 0x100000
-    asmsh.PrintSize = 128 + 32
-    asmsh.PrintMergin = 64
-    asmsh.StackStart = 0x300000
-    asmsh.StackSize   = 2 * 1024 * 1024
-    asmsh.StackAddr = asmsh.StackStart + (asmsh.StackSize / 2)
+// sample: str w11, [x13]; ldrb w15, [x13]; mov x0, 0x37; sub x0,x1,x2
+func SetArm64(bigEndian bool) Machine {
+    var mc Machine
+    mc.bit = 64
+    mc.sp = uc.ARM64_REG_SP
+    mc.bp = uc.ARM64_REG_X29
+    mc.start = 0x0000
 
-    asmsh.UnicornArch = uc.ARCH_ARM64
     // fixme: arm64eb(armebv8) cannot assemble with keystone, but keystone say supported!
     if bigEndian {
-        asmsh.KeystoneArch = keystone.ARCH_ARM
-        asmsh.KeystoneMode = keystone.MODE_ARM + keystone.MODE_BIG_ENDIAN + keystone.MODE_V8
-        asmsh.UnicornMode = uc.MODE_ARM + uc.MODE_BIG_ENDIAN
-        asmsh.Prompt = "(arm64eb)> "
+        mc.ks, _ = keystone.New(keystone.ARCH_ARM, keystone.MODE_ARM + keystone.MODE_BIG_ENDIAN + keystone.MODE_V8)
+        mc.mu, _ = uc.NewUnicorn(uc.ARCH_ARM64, uc.MODE_ARM + uc.MODE_BIG_ENDIAN)
+        mc.oldMu, _ = uc.NewUnicorn(uc.ARCH_ARM64, uc.MODE_ARM + uc.MODE_BIG_ENDIAN)
+        mc.Prompt = "(arm64eb)> "
     } else {
-        asmsh.KeystoneArch = keystone.ARCH_ARM64
-        asmsh.KeystoneMode = keystone.MODE_LITTLE_ENDIAN
-        asmsh.UnicornMode = uc.MODE_ARM
-        asmsh.Prompt = "(arm64)> "
+        mc.ks, _ = keystone.New(keystone.ARCH_ARM64, keystone.MODE_LITTLE_ENDIAN)
+        mc.mu, _ = uc.NewUnicorn(uc.ARCH_ARM64, uc.MODE_ARM + uc.MODE_LITTLE_ENDIAN)
+        mc.oldMu, _ = uc.NewUnicorn(uc.ARCH_ARM64, uc.MODE_ARM + uc.MODE_LITTLE_ENDIAN)
+        mc.Prompt = "(arm64)> "
     }
-    asmsh.SavedCtx = nil
-    asmsh.SavedStackSize = 256
-    asmsh.SavedStack = make([]byte, asmsh.SavedStackSize)
-    for i := uint64(0); i < asmsh.SavedStackSize; i++ {
-        asmsh.SavedStack[i] = 0xFF
+
+    mc.mu.MemMap(0x0000, 0x2000)
+    mc.mu.RegWrite(mc.sp, 0x1000)
+    mc.mu.RegWrite(mc.bp, 0x8000)
+
+    mc.oldCtx, _ = mc.mu.ContextSave(nil)
+
+    mc.regOrder = []string{
+        "x0", " x8", "x1", " x9",
+        "x2", "x10", "x3", "x11",
+        "x4", "x12", "x5", "x13",
+        "x6", " sp", "x7", " pc",
+        "nzcv",
     }
-    asmsh.RegOrder = []string{"x0", " x8", "x1", " x9", "x2", "x10", "x3", "x11", "x4", "x12", "x5", "x13", "x6", " sp", "x7", " pc", "nzcv"}
-    asmsh.Regs = map[string]int{
+    mc.regs = map[string]int{
         "x0"    : uc.ARM64_REG_X0,
         "x1"    : uc.ARM64_REG_X1,
         "x2"    : uc.ARM64_REG_X2,
@@ -65,12 +70,11 @@ func SetArm64(asmsh *as.AsmShell, bigEndian bool) {
         "x26"   : uc.ARM64_REG_X26,
         "x27"   : uc.ARM64_REG_X27,
         "x28"   : uc.ARM64_REG_X28,
-        "x29"   : uc.ARM64_REG_X29,
+        "x29"   : uc.ARM64_REG_X29, // fix: frame(base) pointer?
         "x30"   : uc.ARM64_REG_X30,
         " sp"   : uc.ARM64_REG_SP,
         " pc"   : uc.ARM64_REG_PC,
         "nzcv"  : uc.ARM64_REG_NZCV,
     }
-    asmsh.SP = uc.ARM64_REG_SP
-    asmsh.PrintCtx = asmsh.PrintCtx64
+    return mc
 }
