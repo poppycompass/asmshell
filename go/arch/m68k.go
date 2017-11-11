816a39363029c874c84320204d2cf0d4c3c7c575
poppycompass <t0g0v31dk@gmail.com>
2017-11-11 12:54:46 +0900

    fix m68k.go, but does not use

--- go/arch/m68k.go
+++ go/arch/m68k.go
@@ -3,33 +3,29 @@ package arch
 import (
     "github.com/keystone-engine/keystone/bindings/go/keystone"
     uc "github.com/unicorn-engine/unicorn/bindings/go/unicorn"
-    as "github.com/poppycompass/asmshell/go"
 )
 
 // keystone is not supported, rasm2 supports only disassembler
-func SetM68k(asmsh *as.AsmShell) {
-    asmsh.CodeAddr  = 0x100000
-    asmsh.PrintSize = 64 + 16
-    asmsh.PrintMergin = 32
-    asmsh.StackStart = 0x300000
-    asmsh.StackSize   = 2 * 1024 * 1024
-    asmsh.StackAddr = asmsh.StackStart + (asmsh.StackSize / 2)
+func SetM68k() Machine {
+    var mc Machine
+    mc.bit = 32
+    mc.sp = uc.M68K_REG_A7
+    mc.bp = uc.M68K_REG__SR // correct?
+    mc.start = 0x0000
 
-    //asmsh.KeystoneArch = keystone.ARCH_X86
-    //asmsh.KeystoneMode = keystone.MODE_32
-    //asmsh.KeystoneOPTType = keystone.OPT_SYNTAX
-    //asmsh.KeystoneOPTVal = keystone.OPT_SYNTAX_INTEL
-    asmsh.UnicornArch = uc.ARCH_M68K
-    asmsh.UnicornMode = uc.MODE_BIG_ENDIAN
-    asmsh.SavedCtx = nil
-    asmsh.SavedStackSize = 256
-    asmsh.SavedStack = make([]byte, asmsh.SavedStackSize)
-    for i := uint64(0); i < asmsh.SavedStackSize; i++ {
-        asmsh.SavedStack[i] = 0xFF
+    //mc.ks, _ = keystone.New(keystone.ARCH_ARM, keystone.MODE_ARM + keystone.MODE_BIG_ENDIAN)
+    mc.mu, _ = uc.NewUnicorn(uc.ARCH_M68K, uc.MODE_BIG_ENDIAN)
+    mc.oldMu, _ = uc.NewUnicorn(uc.ARCH_M68K, uc.MODE_BIG_ENDIAN)
+    mc.Prompt = "(m68k)> "
+
+    mc.regOrder = []string{
+        "d0", "a0", "d1", "a1",
+        "d2", " a2", "d3", "a3",
+        "d4", "a4", "d5", "a5",
+        "d6", "a6", "d7", "a7/sp",
+        "pc", "sr",
     }
-    asmsh.Prompt = "(m68k)> "
-    asmsh.RegOrder = []string{"d0", "a0", "d1", "a1", "d2", " a2", "d3", "a3", "d4", "a4", "d5", "a5", "d6", "a6", "d7", "a7/sp", "pc", "sr"}
-    asmsh.Regs = map[string]int{
+    mc.regs = map[string]int{
         "d0"    : uc.M68K_REG_D0,
         "d1"    : uc.M68K_REG_D1,
         "d2"    : uc.M68K_REG_D2,
@@ -49,6 +45,5 @@ func SetM68k(asmsh *as.AsmShell) {
         "pc"    : uc.M68K_REG_PC,
         "sr"    : uc.M68K_REG_SR,
     }
-    asmsh.SP = uc.M68K_REG_A7
-    asmsh.PrintCtx = asmsh.PrintCtx32
+    return mc
 }
