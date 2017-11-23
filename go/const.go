7016a044213b849b7f7b190287f7547eaadcb09d
poppycompass <t0g0v31dk@gmail.com>
2017-11-23 23:34:31 +0900

    disable arm64be

--- go/const.go
+++ go/const.go
@@ -2,7 +2,7 @@ package main
 
 const (
     VERSION   string = "0.1.0"
-    SUPPORTED string = "i8086, x86, x64, arm-thumb(be), arm(be), arm64(be), mips(be), mips64(be), sparc(le), sparc64, [ppc|powerpc], [ppc64(le)|powerpc64(le)], [sysz|systemz|systemZ]"
+    SUPPORTED string = "i8086, x86, x64, arm-thumb(be), arm(be), arm64, mips(be), mips64(be), sparc(le), sparc64, [ppc|powerpc], [ppc64(le)|powerpc64(le)], [sysz|systemz|systemZ]"
     BUFSIZE uint64 = 1024
 )
 
