8f5242ece558be45046175230c228f3fc1de26b6
poppycompass <t0g0v31dk@gmail.com>
2017-12-21 14:01:03 +0900

    change version
    * Complete support for Linux/OSX build

--- go/const.go
+++ go/const.go
@@ -1,7 +1,7 @@
 package main
 
 const (
-    VERSION   string = "0.1.0"
+    VERSION   string = "0.1.2"
     SUPPORTED string = "i8086, x86, x64, thumb(be), arm(be), arm64, mips(be), mips64(be), sparc(le), sparc64, [ppc|powerpc], [ppc64(le)|powerpc64(le)], [sysz|systemz|systemZ]"
     BUFSIZE uint64 = 1024
 )
